f9b6c768f9ad1618bd6e9772e5975ea4b0a3c0a2
Kaden Ho <kadenho@shyftsolutions.io>
2026-06-05 21:23:40 +0000

    Move window to top when changing pages

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './global.css'
 import Layout from './layout.jsx'
+import ScrollToTop from './ScrollToTop'
 import Home from './pages/home/home.jsx'
 import Portfolio from './pages/portfolio/portfolio.jsx'
 import CV from './pages/cv/cv.jsx'
@@ -11,6 +12,7 @@ import About from './pages/about/about.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
