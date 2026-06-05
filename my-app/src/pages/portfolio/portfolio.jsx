91049e25822c97085159e974546b965a00cf8938
Kaden Ho <kadenho@shyftsolutions.io>
2026-06-05 18:29:15 +0000

    Add modal panels

--- my-app/src/pages/portfolio/portfolio.jsx
+++ my-app/src/pages/portfolio/portfolio.jsx
@@ -1,7 +1,26 @@
+import VAI1Cover from './assets/via-1/cover.png';
+import ZodiakCover from './assets/zodiak/cover.png';
+import BlueBellValleyCover from './assets/bluebell-valley/cover.png';
+import LunCover from './assets/lun/cover.png';
+import './portfolio.css'
+
 export default function Portfolio() {
   return (
     <main className="page-content">
-      <h1>Portfolio</h1>
+      <section id="modal-panels" className="modal-panels">
+        <button id="via-1-modal" class="modal-panel">
+          <img src={VAI1Cover} alt="VIA-1 poster" class="modal-panel-image" draggable={false}></img>
+        </button>
+        <button id="zodiak-modal" class="modal-panel">
+          <img src={ZodiakCover} alt="Zodiak circle" class="modal-panel-image" draggable={false}></img>
+        </button>
+        <button id="bluebell-valley-modal" class="modal-panel">
+          <img src={BlueBellValleyCover} alt="Blue Bell Valley poster" class="modal-panel-image" draggable={false}></img>
+        </button>
+        <button id="lun-modal" class="modal-panel">
+          <img src={LunCover} alt="Lun poster" class="modal-panel-image" draggable={false}></img>
+        </button>
+      </section>
     </main>
   )
 }
