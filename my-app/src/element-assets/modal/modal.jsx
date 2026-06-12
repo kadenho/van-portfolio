d4082803ff9ae2a4785c44ad28dc32c81b38ce1c
Kaden Ho <kho7@huskers.unl.edu>
2026-06-12 19:50:36 +0000

    Add caption and date to modal

--- my-app/src/element-assets/modal/modal.jsx
+++ my-app/src/element-assets/modal/modal.jsx
@@ -13,7 +13,15 @@ export default function Modal({ project, onClose }) {
         <div className="modal-top-bar">
           <h2>{project.title}</h2>
         </div>
-        <Carousel slides={project.images} />
+        <div className="modal-content">
+          <Carousel slides={project.images} />
+          <div className="modal-sidebar">
+            <div className="modal-date">
+              <small>Date: {project.date}</small>
+            </div>
+            <small className="modal-caption">{project.caption}</small>
+          </div>
+        </div>
       </dialog>
     </div>
   )
